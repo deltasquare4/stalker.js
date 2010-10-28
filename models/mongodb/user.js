b97d8806e003a8ce6cc7cf4a3f69f5a333c80821
Christoph Jasinski <christoph.jasinski@googlemail.com>
2010-10-28 18:13:55 +0200

    extended user model

--- models/mongodb/user.js
+++ models/mongodb/user.js
@@ -3,14 +3,20 @@ var db = driver.connect('mongodb://localhost/stalkerjs');
 
 driver.model('User', {
   
-  properties: [],
+properties: ['login', 'first', 'last','sex','city', 'country', 'email', 'memeber_since', 'is_active', 'posts_counter', 'updated_at','imageUri', 'bio', {'skills': []}],
 
-  indexes: [],
+  indexes: ['login', [{'username':1},{unique: true}]],
 
   methods: {
     save: function(fn){
       this.updated_at = new Date();
       this.__super__(fn);
+    }
+  },
+
+  getters: {
+    full_name: function(){
+      return this.first+' '+this.last;
     }
   }
 
